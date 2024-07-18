6a80d3b5d8ebae42e9c70c0df210b293edb0bd0c
jonathanferreirar <jonathanferreirar@gmail.com>
2024-07-18 19:00:58 -0300

    feat: sparkles: add icons library and config icons to use

---
+++ src/app/icons.ts
@@ -0,0 +1,13 @@
+import { FaCheck, FaPlay, FaSearch, FaStop, FaTrash } from 'react-icons/fa'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { RiPencilFill } from 'react-icons/ri'
+
+export const Icons = {
+  burgue: GiHamburgerMenu,
+  check: FaCheck,
+  pencil: RiPencilFill,
+  trash: FaTrash,
+  search: FaSearch,
+  play: FaPlay,
+  stop: FaStop,
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { cn } from '@/lib/utils'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -17,7 +19,16 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main
+          className={cn(
+            'grid grid-cols-2 grid-rows-1 min-h-screen min-w-screen',
+          )}
+        >
+          <div className="h-screen w-1/3 bg-black"> jonathan</div>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
