5d67cc694af643ffc6e269fe30d92a38101ada38
jonathanferreirar <jonathanferreirar@gmail.com>
2024-07-19 03:20:04 -0300

    fix : boom: adjust conflict

--- src/primitive/ui/sheet.tsx
+++ src/primitive/ui/sheet.tsx
@@ -49,7 +49,7 @@ const sheetVariants = cva(
   },
 )
 
-type SheetContentProps = {} & React.ComponentPropsWithoutRef<
+type SheetContentProps = React.ComponentPropsWithoutRef<
   typeof SheetPrimitive.Content
 > &
   VariantProps<typeof sheetVariants>
